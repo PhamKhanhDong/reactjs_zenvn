84aa1fab282ff6c0b5b1b949be73490d1da88999
DONGVV <khanhdong95vip@gmail.com>
2018-07-27 18:02:50 +0700

    init reactjs

---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Hello extends Component {
+  render() {
+    return(
+      <div className="col-lg-12">
+        <a className="btn btn-default btn-success">button</a>
+      </div>
+    )
+  }
+}
